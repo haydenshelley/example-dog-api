7b7454ee45431ff12293b94eedec871596e64853
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-08-08 12:10:49 -0700

    add feature to only allow user to create dog if logged in

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -5,12 +5,16 @@ class DogsController < ApplicationController
   end
 
   def create
-    @dog = Dog.new(
-      name: params[:name],
-      age: params[:age],
-      breed: params[:breed]
-    )
-    @dog.save
-    render :show
+    if current_user
+      @dog = Dog.new(
+        name: params[:name],
+        age: params[:age],
+        breed: params[:breed]
+      )
+      @dog.save
+      render :show
+    else
+      render json: {message: "User must be logged in to create a new dog"}
+    end
   end
 end
